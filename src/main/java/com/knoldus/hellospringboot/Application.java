eec91fcc1517e75e9979e375feebef34625a6d46
Azmat Hasan <azmat.hasan@knoldus.com>
2020-12-04 11:55:54 +0530

    Update Message

--- src/main/java/com/knoldus/hellospringboot/Application.java
+++ src/main/java/com/knoldus/hellospringboot/Application.java
@@ -21,7 +21,7 @@ public class Application {
 
     @Bean
     RouterFunction<ServerResponse> routes() {
-        return route(GET("/api/hello"), request -> ok().syncBody("Hello, SpringBootDemo!"));
+        return route(GET("/api/hello"), request -> ok().syncBody("Hello, SpringBootDemoFinal!"));
     }
 
     public static void main(String[] args) {
